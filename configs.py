a80261a8a3b2580a01bc22350b7334585dc98f59
Alex He <ahe4264@hotmail.com>
2024-03-30 20:19:09 -0400

    Update API Key again

--- configs.py
+++ configs.py
@@ -4,7 +4,7 @@ PORT = '8000'
 DEBUG = True
 
 OPENAI_COMPLETIONS_V1_URL = "https://api.openai.com/v1/completions"
-OPEN_AI_API_KEY = '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'
+OPEN_AI_API_KEY = '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'
 SPEECHSUPER_APPKEY = "1709716879000299"
 SPEECHSUPER_SECRETKEY = "41cb3809a1e5ffbd7b4cdce0e104e1a3"
 
